91f35117adf07f7ded7a8c7e18745e68ea40de0b
alamdave <alamdave@gmail.com>
2024-08-10 22:11:22 +0100

    Added SideNav and working functions

---
+++ portfolio/src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div id="home">
+      <img
+        className=""
+        src="https://png.pngtree.com/thumb_back/fh260/background/20210324/pngtree-abstract-playful-portfolio-pink-memphis-image_593415.jpg"
+      ></img>
+    </div>
+  );
+};
+
+export default Home;
