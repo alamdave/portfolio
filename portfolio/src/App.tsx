f78770875031987c1529a062132429fa64e6a217
alamdave <alamdave@gmail.com>
2024-08-11 20:50:41 +0100

    Added scrolling functionality to carousel

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -7,14 +7,17 @@ import ImageTrack from "./components/ImageTrack";
 function App() {
   return (
     <>
-      <div className="card overflow-hidden h-screen">
-        <div className="bg-hero-pattern w-full h-screen bg-repeat ">
-          <ImageTrack />
-        </div>
-        <footer className="bg-footer-texture py-4"></footer>
-        <SideNav />
-        <Home />
-      </div>
+      <head></head>
+      <main className="bg-hero-pattern w-full h-screen bg-repeat px-10">
+        <section className="min-h-screen">
+          <SideNav />
+          <div className="card overflow-hidden h-screen">
+            <ImageTrack />
+            <footer className="bg-footer-texture py-4"></footer>
+            <Home />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
--- portfolio/src/components/ImageTrack.tsx
+++ portfolio/src/components/ImageTrack.tsx
@@ -10,6 +10,7 @@ const ImageTrack: React.FC = () => {
   ];
 
   const trackRef = useRef<HTMLDivElement>(null);
+  let isAnimating = false;
 
   useEffect(() => {
     const track = trackRef.current;
@@ -33,22 +34,72 @@ const ImageTrack: React.FC = () => {
       const maxDelta = window.innerWidth / 2;
 
       const percentage = (mouseDelta / maxDelta) * -100;
-      const unp = parseFloat(track.dataset.prevPercentage || "0") + percentage;
-      let nextPercentage = Math.max(Math.min(unp, 0), -100);
+      const unweightedNextPercentage =
+        parseFloat(track.dataset.prevPercentage || "0") + percentage;
+      let nextPercentage = Math.max(
+        Math.min(unweightedNextPercentage, 0),
+        -100
+      );
 
       track.dataset.percentage = `${nextPercentage}`;
 
-      track.animate(
-        { transform: `translate(${nextPercentage}%, -75%)` },
-        { duration: 1200, fill: "forwards" }
-      );
+      if (!isAnimating) {
+        isAnimating = true;
+        requestAnimationFrame(() => {
+          track.animate(
+            { transform: `translate(${nextPercentage}%, -50%)` },
+            { duration: 1200, fill: "forwards" }
+          );
 
-      const images = track.getElementsByClassName("image");
-      for (const image of images) {
-        (image as HTMLElement).animate(
-          { objectPosition: `${100 + nextPercentage}% center` },
-          { duration: 1200, fill: "forwards" }
-        );
+          const images = track.getElementsByClassName("image");
+          for (const image of images) {
+            (image as HTMLElement).animate(
+              { objectPosition: `${100 + nextPercentage}% center` },
+              { duration: 1200, fill: "forwards" }
+            );
+          }
+          isAnimating = false;
+        });
+      }
+    };
+
+    const handleScroll = (e: WheelEvent) => {
+      e.preventDefault();
+      const deltaY = e.deltaY;
+      const prevPercentage = parseFloat(track.dataset.percentage || "0");
+
+      let nextPercentage =
+        prevPercentage + (deltaY / window.innerHeight) * -100;
+      nextPercentage = Math.max(Math.min(nextPercentage, 0), -100);
+
+      track.dataset.percentage = `${nextPercentage}`;
+
+      if (!isAnimating) {
+        isAnimating = true;
+        requestAnimationFrame(() => {
+          track.animate(
+            { transform: `translate(${nextPercentage}%, -50%)` },
+            { duration: 1200, fill: "forwards" }
+          );
+
+          const images = track.getElementsByClassName("image");
+          for (const image of images) {
+            (image as HTMLElement).animate(
+              { objectPosition: `${100 + nextPercentage}% center` },
+              { duration: 1200, fill: "forwards" }
+            );
+          }
+          isAnimating = false;
+        });
+      }
+
+      // Allow normal scrolling if the track is at either end
+      if (nextPercentage === 0 || nextPercentage === -100) {
+        window.removeEventListener("wheel", handleScroll);
+        window.scrollBy(0, deltaY);
+        setTimeout(() => {
+          window.addEventListener("wheel", handleScroll, { passive: false });
+        }, 1200);
       }
     };
 
@@ -58,6 +109,7 @@ const ImageTrack: React.FC = () => {
     window.addEventListener("touchstart", handleMouseDown);
     window.addEventListener("touchend", handleMouseUp);
     window.addEventListener("touchmove", handleMouseMove);
+    window.addEventListener("wheel", handleScroll, { passive: false });
 
     return () => {
       window.removeEventListener("mousedown", handleMouseDown);
@@ -66,6 +118,7 @@ const ImageTrack: React.FC = () => {
       window.removeEventListener("touchstart", handleMouseDown);
       window.removeEventListener("touchend", handleMouseUp);
       window.removeEventListener("touchmove", handleMouseMove);
+      window.removeEventListener("wheel", handleScroll);
     };
   }, []); // Empty dependency array to run only once on mount
 
@@ -73,7 +126,7 @@ const ImageTrack: React.FC = () => {
     <div
       ref={trackRef}
       id="ImageTrack"
-      className="flex gap-[2vmin] absolute left-[30%] top-[50%] translate-x-[0%] translate-y-[-50%]"
+      className="flex gap-[2vmin] absolute left-[10%] top-[40%] translate-x-[0%] translate-y-[-50%]"
       data-mouse-down-at="0"
       data-prev-percentage="0"
     >
@@ -82,7 +135,7 @@ const ImageTrack: React.FC = () => {
           key={index}
           src={image}
           alt={`image ${index + 1}`}
-          className="image w-[32vmin] h-[46vmin] object-cover object-left-[%50] object-right-[%50]"
+          className="image w-[32vmin] h-[46vmin] object-cover object-left-[50%] object-right-[50%]"
           draggable="false"
         />
       ))}
