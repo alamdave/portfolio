51a772bb780fe5d5b73fbe64570c7c15bea8e816
alamdave <alamdave@gmail.com>
2024-09-16 13:31:27 +0100

    scroll bars are now hidden

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import Bio from "../components/Bio";
-import MainLayout from "../layouts/MainLayout";
 import ImageTrack from "../components/ImageTrack";
-import ImageCarousel from "../components/ImageCarousel";
 
 const images = [
   "https://images.unsplash.com/photo-1550007345-dcdff81aa558?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -15,12 +13,6 @@ const images = [
   "https://images.unsplash.com/photo-1723145886817-1a2ee70a251b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1723083661302-ca5b3459e278?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
-/*      <section className="image-track-section">
-<ImageTrack images={images} />
-</section>
-<section className="image-track-section2">
-  <ImageTrack images={images} />
-</section> */
 
 const Home: React.FC = () => {
   return (
